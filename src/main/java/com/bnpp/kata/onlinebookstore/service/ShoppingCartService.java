bbc07bfe78a3f07120713e23e39691848305c739
reshm <reshmapsuresh20@gmail.com>
2024-12-06 20:50:11 +0530

    feat : Check user have entry in shoppingcart

--- src/main/java/com/bnpp/kata/onlinebookstore/service/ShoppingCartService.java
+++ src/main/java/com/bnpp/kata/onlinebookstore/service/ShoppingCartService.java
@@ -1,13 +1,27 @@
 package com.bnpp.kata.onlinebookstore.service;
 
+import com.bnpp.kata.onlinebookstore.entity.ShoppingCart;
+import com.bnpp.kata.onlinebookstore.repository.ShoppingCartRepository;
 import com.bnpp.kata.onlinebookstore.store.CartResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import java.util.Collections;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class ShoppingCartService {
+
+    private final ShoppingCartRepository shoppingCartRepository;
+
     public List<CartResponse> getCartItems (Long userId) {
-        return Collections.emptyList();
+
+        ShoppingCart shoppingCart = shoppingCartRepository.findByUserId(userId)
+                .orElse(null);
+
+        if (shoppingCart == null) {
+            return Collections.emptyList();
+        }
+        return (List<CartResponse>) CartResponse.builder().build();
     }
 }
--- src/test/java/com/bnpp/kata/onlinebookstore/service/ShoppingCartServiceTest.java
+++ src/test/java/com/bnpp/kata/onlinebookstore/service/ShoppingCartServiceTest.java
@@ -1,5 +1,9 @@
 package com.bnpp.kata.onlinebookstore.service;
 
+import com.bnpp.kata.onlinebookstore.entity.ShoppingCart;
+import com.bnpp.kata.onlinebookstore.entity.Users;
+import com.bnpp.kata.onlinebookstore.repository.ShoppingCartRepository;
+import com.bnpp.kata.onlinebookstore.repository.UserRepository;
 import com.bnpp.kata.onlinebookstore.store.CartResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,6 +19,12 @@ public class ShoppingCartServiceTest {
     @Autowired
     private ShoppingCartService shoppingCartService;
 
+    @Autowired
+    private ShoppingCartRepository shoppingCartRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
     @Test
     @DisplayName("Cart Details : No cart available , return empty list ")
     void getCartItems_noItemsAddedToCart_returnsEmptyList() {
@@ -22,5 +32,19 @@ public class ShoppingCartServiceTest {
         List<CartResponse> result = shoppingCartService.getCartItems (USERID);
 
         assertThat(result.isEmpty ());
+    }
+
+    @Test
+    @DisplayName("Cart Details : Check user have entry in shopping cart")
+    void getCartItems_checkUserHaveShoppingEcartEntry_returnsReponse() {
+
+        userRepository.save(Users.builder().username(USERNAME).password(PASSWORD).build());
+        Users user = userRepository.findByUsername (USERNAME);
+        ShoppingCart shoppingCartUser = ShoppingCart.builder ().user (user).build();
+        shoppingCartRepository.save(shoppingCartUser);
+
+        List<CartResponse> result = shoppingCartService.getCartItems (USERID);
+
+        assertThat(!result.isEmpty ());
     }
 }
