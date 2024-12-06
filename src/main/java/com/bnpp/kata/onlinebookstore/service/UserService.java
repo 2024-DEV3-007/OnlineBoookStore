434d715ac8f0e09790664876a05fe9b0afb4a669
reshm <reshmapsuresh20@gmail.com>
2024-12-06 15:15:18 +0530

    feat : New User details saved in db

--- src/main/java/com/bnpp/kata/onlinebookstore/service/UserService.java
+++ src/main/java/com/bnpp/kata/onlinebookstore/service/UserService.java
@@ -1,13 +1,28 @@
 package com.bnpp.kata.onlinebookstore.service;
 
+import com.bnpp.kata.onlinebookstore.entity.Users;
+import com.bnpp.kata.onlinebookstore.repository.UserRepository;
 import com.bnpp.kata.onlinebookstore.store.UserLoginRequest;
 import com.bnpp.kata.onlinebookstore.store.UserLoginResponse;
 import static com.bnpp.kata.onlinebookstore.constants.Constants.*;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
     public UserLoginResponse registerUser (UserLoginRequest registerRequest) {
+
+        Users user = Users.builder ().username (registerRequest.getUsername ())
+                .firstname (registerRequest.getFirstName ())
+                .lastname (registerRequest.getLastName ())
+                .password (registerRequest.getPassword ())
+                .build ();
+
+        userRepository.save (user);
 
         return UserLoginResponse.builder()
                 .message(REGISTER_SUCCESS)
--- src/test/java/com/bnpp/kata/onlinebookstore/service/UserServiceTest.java
+++ src/test/java/com/bnpp/kata/onlinebookstore/service/UserServiceTest.java
@@ -1,21 +1,25 @@
 package com.bnpp.kata.onlinebookstore.service;
 
+import com.bnpp.kata.onlinebookstore.entity.Users;
+import com.bnpp.kata.onlinebookstore.repository.UserRepository;
 import com.bnpp.kata.onlinebookstore.store.UserLoginRequest;
 import com.bnpp.kata.onlinebookstore.store.UserLoginResponse;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static com.bnpp.kata.onlinebookstore.constants.TestConstants.*;
 
+@SpringBootTest
 public class UserServiceTest {
 
-    private UserService UserService;
+    @Autowired
+    private UserService userService;
 
-    @BeforeEach
-    public void setup() {
-        UserService = new UserService();
-    }
+    @Autowired
+    private UserRepository userRepository;
 
     @Test
     @DisplayName ("Register New User : User Registration Successful")
@@ -27,8 +31,24 @@ public class UserServiceTest {
                 .lastName (LASTNAME)
                 .password (PASSWORD).build ();
 
-        UserLoginResponse result = UserService.registerUser (userLoginRequest);
+        UserLoginResponse result = userService.registerUser (userLoginRequest);
 
         assertThat (result.getMessage ()).isEqualTo (REGISTER_SUCCESS);
+    }
+
+    @Test
+    @DisplayName ("Register New User : User details should be saved in db")
+    void registerUser_userDetailsSaveInDB_returnsSuccessResponse() {
+
+        UserLoginRequest userLoginRequest = UserLoginRequest.builder ()
+                .username (USERNAME)
+                .firstName (FIRSTNAME)
+                .lastName (LASTNAME)
+                .password (PASSWORD).build ();
+
+        UserLoginResponse result = userService.registerUser (userLoginRequest);
+
+        Users savedUser = userRepository.findByUsername(USERNAME);
+        assertThat (savedUser.getUsername ()).isEqualTo (USERNAME);
     }
 }
