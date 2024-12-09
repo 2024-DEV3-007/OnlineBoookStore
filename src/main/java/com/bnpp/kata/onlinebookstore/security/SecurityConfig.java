2e5493e74c8c6971210b0529f9611b174e4bb3f9
reshm <reshmapsuresh20@gmail.com>
2024-12-09 10:22:57 +0530

    feat :All Cors Enabling for the client

--- src/main/java/com/bnpp/kata/onlinebookstore/security/SecurityConfig.java
+++ src/main/java/com/bnpp/kata/onlinebookstore/security/SecurityConfig.java
@@ -1,17 +1,33 @@
 package com.bnpp.kata.onlinebookstore.security;
 
+import com.bnpp.kata.onlinebookstore.service.UserAuthenticationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig implements WebMvcConfigurer {
 
+    @Autowired
+    private UserAuthenticationService userAuthenticationService;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder ();
@@ -21,6 +37,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeHttpRequests()
+                .antMatchers (HttpMethod.OPTIONS, "/**").permitAll ()
                 .antMatchers("/api/register","/api/login").permitAll()
                 .antMatchers( "/api/books","/api/cart/**").authenticated ()
                 .anyRequest().authenticated()
